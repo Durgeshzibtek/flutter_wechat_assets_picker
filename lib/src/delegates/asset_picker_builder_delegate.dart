a9b7fe8d98fb90165088892f04d4130d22cfb27b
Durgesh <durgesh@zibtek.in>
2023-01-10 18:20:24 +0530

    Date check

--- lib/src/delegates/asset_picker_builder_delegate.dart
+++ lib/src/delegates/asset_picker_builder_delegate.dart
@@ -2192,7 +2192,7 @@ class DefaultAssetPickerBuilderDelegate extends AssetPickerBuilderDelegate<Asset
       ),
     ) : PositionedDirectional(
       //start: 46,
-      start: MediaQuery.of(context).size.width * 0.2,
+      start: MediaQuery.of(context).size.width * 0.15,
       end: 0,
       bottom: 0,
       top: 5,
