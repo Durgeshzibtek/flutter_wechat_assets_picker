d63a49286e0f2098c91530564684e9f8d3fe8cf4
Durgesh <durgesh@zibtek.in>
2023-01-25 18:27:51 +0530

    Change in postions again

--- lib/src/delegates/asset_picker_builder_delegate.dart
+++ lib/src/delegates/asset_picker_builder_delegate.dart
@@ -1990,7 +1990,7 @@ class DefaultAssetPickerBuilderDelegate extends AssetPickerBuilderDelegate<Asset
             child: selectorWidget,
           ) : Positioned(
               top: 0,
-              right: -10,
+              right: -5,
               child: selectorWidget,
             );
         }
